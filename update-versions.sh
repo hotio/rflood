ee37cbaf8809b3c462ca0cac76e1300e1777ce2f
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 09:38:04 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
-rtorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/rtorrent/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${rtorrent_version} ]] && exit 0
-flood_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${flood_version} ]] && exit 0
+set -e
+rtorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/rtorrent/releases/latest" | jq -re .tag_name)
+flood_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name)
 version="${rtorrent_version}--${flood_version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .flood_version = "'"${flood_version}"'" | .rtorrent_version = "'"${rtorrent_version}"'"' <<< "${version_json}" > VERSION.json
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg flood_version "${flood_version//v/}" \
+    --arg rtorrent_version "${rtorrent_version//v/}" \
+    '.version = $version | .flood_version = $flood_version | .rtorrent_version = $rtorrent_version' <<< "${json}" | tee VERSION.json
