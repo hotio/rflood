cbcfadbe108bcfee61343eb6a1b195715e56f695
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-13 10:11:37 +0100

    rename args [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,9 +2,9 @@
 set -exuo pipefail
 
 version=$(curl -fsSL "https://api.github.com/repos/rakshasa/rtorrent/releases/latest" | jq -re .tag_name)
-flood_version=$(curl -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name)
+version_flood=$(curl -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name)
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg flood_version "${flood_version//v/}" \
-    '.version = $version | .flood_version = $flood_version' <<< "${json}" | tee meta.json
+    --arg version_flood "${version_flood//v/}" \
+    '.version = $version | .version_flood = $version_flood' <<< "${json}" | tee meta.json
